ce2684c20c6f58ad3b688365673352571c29a4a9
Gabe Gerdes <68089160+GabeGerdes@users.noreply.github.com>
2021-03-13 17:52:27 -0600

    uploaded files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppGerdes.Models;
+
+namespace FirstResponsiveWebAppGerdes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.YOB = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeFinder model) 
+        {
+            ViewBag.YOB = model.AgeThisYear();
+            return View(model);
+        }
+    }
+}
---
+++ Models/AgeFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FirstResponsiveWebAppGerdes.Models
+{
+    public class AgeFinder
+    {
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please use a number.")]
+        [Range(1900,2021, ErrorMessage ="Please use a number between 2021 and 1900.")]
+        public int? BirthYear { get; set; }
+
+        public int? AgeThisYear()
+        {
+            int? age = 0;
+            const int thisYear = 2021;
+            age = thisYear - BirthYear;
+            return age;
+        }
+    }
+}
